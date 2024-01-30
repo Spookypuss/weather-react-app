548fa8b4b2c6969dded20b4d1205a4a1918ce98c
Spookypuss <114418331+Spookypuss@users.noreply.github.com>
2024-01-30 16:28:17 +0000

    Added footer with link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather</h1>
       <Weather />
+      <br />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>This app is coded by Lyn Rycroft and is <a href="https://github.com/Spookypuss/weather-react-app" target="_blank" rel="noreferrer">open source</a>.</footer>
+        </div>
+    )
+}
